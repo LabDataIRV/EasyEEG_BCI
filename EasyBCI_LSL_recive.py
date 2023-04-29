cab8db1e5202b825af042449467fcece77b201d3
LabDataIRV <96665379+LabDataIRV@users.noreply.github.com>
2023-04-29 22:43:38 +0300

    Update EasyBCI_LSL_recive.py

--- EasyBCI_LSL_recive.py
+++ EasyBCI_LSL_recive.py
@@ -1,17 +1,17 @@
-"""Пример программы для получения многоканальных данных по протоколу LSL"""
+"""Пример программы для получения данных с приложения EasyEEG BCI по протоколу LSL"""
 
 from pylsl import StreamInlet, resolve_stream
 
 def main():
     print("Ожидание данных...")
-    streams = resolve_stream('type', 'EEG') # Ищем нужный сигнал
+    streams = resolve_stream('type', 'EEG')
     # Создаем вход для получения данных
     inlet = StreamInlet(streams[0])
 
     while True:
         # Получение нового измерения
         sample, timestamp = inlet.pull_sample()
-        print(timestamp, sample)
+        print(sample)
 
 if __name__ == '__main__':
     main()
